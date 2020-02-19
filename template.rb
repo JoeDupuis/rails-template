74cff971811b25bbfb27819e39fbc6d26564521d
Joe Dupuis <joe.dupuis@xylio.net>
2020-02-18 21:53:28 -0500

    Initial commit

---
+++ template.rb
@@ -0,0 +1,26 @@
+
+def apply_template!
+  add_template_repository_to_source_path
+  copy_file "lib/templates/rails/scaffold_controller/controller.rb.tt"
+end
+
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("rails-template-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/mattbrictson/rails-template.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{rails-template/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
+
+apply_template!
