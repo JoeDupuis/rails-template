f5b68b33c2f404236a36d55950972893e55bcc1a
Joe Dupuis <joe@dupuis.io>
2020-04-08 20:21:15 -0400

    Better form error

--- config/initializers/customize_error.rb
+++ config/initializers/customize_error.rb
@@ -1,9 +1,9 @@
 ActionView::Base.field_error_proc = proc do |html_tag, _instance|
   class_attr_index = html_tag.index 'class="'
-
+  class_attr_index  = nil if class_attr_index.try :>, html_tag.index('>') #support label wrapping field
   if class_attr_index
-    html_tag.insert class_attr_index + 7, 'field--with-error '
+    html_tag.insert class_attr_index + 7, 'field_with_errors '.html_safe
   else
-    html_tag.insert html_tag.index('>'), ' class="field--with-error"'
+    html_tag.insert html_tag.index('>'), ' class="field_with_errors"'.html_safe
   end
 end
